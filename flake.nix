70ee2e78078eb17d74b8080551cee14936c3f36f
heidi <h.nyula@gmail.com>
2026-01-03 16:34:01 +0100

    nix setup

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+  # https://flake.parts/module-arguments.html
+  flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
+    imports = [
+      # Optional: use external flake logic, e.g.
+      # inputs.foo.flakeModules.default
+      inputs.process-compose-flake.flakeModule
+    ];
+    flake = {
+      # Put your original flake attributes here.
+    };
+    systems = [
+      # systems for which you want to build the `perSystem` attributes
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      # ...
+    ];
+    perSystem = { config, pkgs, ... }: {
+      # Recommended: move all package definitions here.
+      # e.g. (assuming you have a nixpkgs input)
+      # packages.foo = pkgs.callPackage ./foo/package.nix { };
+      # packages.bar = pkgs.callPackage ./bar/package.nix {
+      #   foo = config.packages.foo;
+      # };
+      devShells.default = let dev = pkgs.writeShellScriptBin "dev" 
+      ''
+        nix run
+      ''; 
+      in pkgs.mkShell {
+        buildInputs = [ pkgs.ruby pkgs.libyaml pkgs.watchman dev ];
+      };
+
+      process-compose.default = {
+        imports = [
+          inputs.services-flake.processComposeModules.default
+        ];
+        settings.processes = {
+          server.command = "bin/rails s";
+          css.command = "bin/rails tailwindcss:watch";
+          css.is_tty = true;
+          setup.command = "bin/setup";
+          setup.depends_on.pg.condition = "process_healthy";
+          server.depends_on.setup.condition = "process_completed";
+        };
+        services = {
+          postgres = { 
+            pg = {
+              enable = true;
+              package = pkgs.postgresql_16;
+
+              port = 6666;
+              listen_addresses = "localhost";
+
+              initialDatabases = [
+                {
+                  # development database
+                  name = "speedpastes_development";
+                }
+                {
+                  # test database
+                  name = "speedpastes_test";
+                }
+              ];
+            };
+          };
+        };
+      };
+    };
+  });
+
+}
