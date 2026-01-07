fc8f480f35346765d1b0320eb83d94261a79e6eb
heidi <h.nyula@gmail.com>
2026-01-08 00:00:06 +0100

    tests are broken idk why i'll fix it later

---
+++ test/integration/admin_actions_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
+    setup do
+        @user = users(:regular)
+        @admin = users(:admin)
+        @other = users(:other)
+    end
+    
+    test "non-admin cannot access invite_codes" do
+        sign_in @user
+        get profile_path(@other.username)
+        assert_redirected_to root_path
+        assert_equal "Not authorized", flash[:alert]
+    end
+end
