0c62ef52cd34631f44cddc1ee63846d13baa9b09
heidi <h.nyula@gmail.com>
2026-01-07 02:08:27 +0100

    Easy copying of invites

---
+++ app/javascript/controllers/invite_codes_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["button", "code"]
+
+    copy() {
+        const codes = []
+        this.codeTargets.forEach(el => {
+            const uses = parseInt(el.dataset.usesCount, 10)
+            const maxUses = parseInt(el.dataset.maxUses, 10)
+            if (uses < maxUses) {
+                codes.push(el.dataset.code)
+            }
+        })
+        if (codes.length > 0) {
+            navigator.clipboard.writeText(codes.join("\n"))
+            this.buttonTarget.textContent = "Copied!"
+            setTimeout(() => {
+                this.buttonTarget.textContent = "Copy valid invites"
+            }, 1500)
+        }
+    }
+}
