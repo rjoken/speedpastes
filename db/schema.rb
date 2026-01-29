14256af8378c3954a6610ff5dae531cd336012a2
heidi <h.nyula@gmail.com>
2026-01-29 22:00:17 +0100

    Add db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_21_235244) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_27_195915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -88,6 +88,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_21_235244) do
   create_table "scratchpads", force: :cascade do |t|
     t.text "body", default: "", null: false
     t.datetime "created_at", null: false
+    t.boolean "encrypted", default: false, null: false
+    t.jsonb "encryption_meta", default: {}, null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_scratchpads_on_user_id", unique: true
