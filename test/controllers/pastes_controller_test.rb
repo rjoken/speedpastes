2dc67060ddb018c4f40d77736670d932a63b07f9
heidi <h.nyula@gmail.com>
2026-02-28 00:12:58 +0100

    nuke tests, I'll rewrite them later :PPPP

--- test/controllers/pastes_controller_test.rb
+++ test/controllers/pastes_controller_test.rb
@@ -1,33 +1,4 @@
 require "test_helper"
 
 class PastesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get pastes_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get pastes_create_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get pastes_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get pastes_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get pastes_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get pastes_destroy_url
-    assert_response :success
-  end
 end
