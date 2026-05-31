8bd692bf85e0e967c2726d842de3a18ca5803d7b
heidi <h.nyula@gmail.com>
2026-05-31 02:27:01 +0200

    Add toggle to show supporter tag on profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_25_122855) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_30_234903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -292,6 +292,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_25_122855) do
     t.string "link"
     t.string "password_digest", null: false
     t.integer "role", default: 0, null: false
+    t.boolean "show_supporter", default: true, null: false
     t.boolean "show_view_count", default: true, null: false
     t.datetime "updated_at", null: false
     t.string "username", null: false
