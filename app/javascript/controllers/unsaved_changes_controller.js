8bcb96c56bb2bdc4155895886dd16497fe2747a8
Heidi <h.nyula@gmail.com>
2026-08-16 15:08:29 +0200

    Add confirmation from navigating away from a create paste page with unsaved changes (#101)
    * Add confirmation from navigating away from a create paste page with data entered
    
    * bundle update
    
    * bundle update update

---
+++ app/javascript/controllers/unsaved_changes_controller.js
@@ -0,0 +1,55 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["title", "body", "tags"];
+
+  connect() {
+    this.initialValues = {
+      title: this.titleTarget.value,
+      body: this.bodyTarget.value,
+      tags: this.tagsTarget.value,
+    };
+
+    this.beforeUnloadHandler = this.onBeforeUnload.bind(this);
+    this.turboBeforeVisitHandler = this.onTurboBeforeVisit.bind(this);
+
+    window.addEventListener("beforeunload", this.beforeUnloadHandler);
+    document.addEventListener(
+      "turbo:before-visit",
+      this.turboBeforeVisitHandler,
+    );
+  }
+
+  disconnect() {
+    window.removeEventListener("beforeunload", this.beforeUnloadHandler);
+    document.removeEventListener(
+      "turbo:before-visit",
+      this.turboBeforeVisitHandler,
+    );
+  }
+
+  onBeforeUnload(event) {
+    if (this.isDirty()) {
+      event.preventDefault();
+    }
+  }
+
+  onTurboBeforeVisit(event) {
+    if (
+      this.isDirty() &&
+      !confirm(
+        "You have unsaved changes. Are you sure you want to leave this page?",
+      )
+    ) {
+      event.preventDefault();
+    }
+  }
+
+  isDirty() {
+    return (
+      this.titleTarget.value !== this.initialValues.title ||
+      this.bodyTarget.value !== this.initialValues.body ||
+      this.tagsTarget.value !== this.initialValues.tags
+    );
+  }
+}
