4e8e2260851828393c0644234e0342bb283c4c2a
heidi <h.nyula@gmail.com>
2026-08-23 23:12:03 +0200

    Add text wrapping to paste editor

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,16 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["menu"]
+  static targets = ["menu"];
 
-    toggle(event) {
-        event.preventDefault()
-        this.menuTarget.classList.toggle("hidden")
-    }
+  toggle(event) {
+    event.preventDefault();
+    this.menuTarget.classList.toggle("hidden");
+  }
 
-    close(event) {
-        if (!this.element.contains(event.target)) {
-            this.menuTarget.classList.add("hidden")
-        }
+  close(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.classList.add("hidden");
     }
+  }
 }
